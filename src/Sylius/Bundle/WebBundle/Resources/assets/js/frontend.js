743e3bf258baba20cbb5c32b32ce76b861f0834f
Diego <songecko@gmail.com>
2014-06-19 17:09:28 -0300

    checkout one step

--- src/Sylius/Bundle/WebBundle/Resources/assets/js/frontend.js
+++ src/Sylius/Bundle/WebBundle/Resources/assets/js/frontend.js
@@ -6,10 +6,54 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+var step = 1;
+
+var checkoutSubmitHandler = function (ev) 
+{
+    $.ajax({
+        type: $(this).attr('method'),
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        success: function (data) 
+        { 	
+        	alert(data);
+        	$('#collapse'+step).collapse('hide');  	
+        	if(data.indexOf('formStep1') >= 0)
+        	{
+        		step = 1;        		
+        	}
+        	else if(data.indexOf('formStep2') >=0)
+        	{
+        		step = 2;
+        	}
+        	else if(data.indexOf('formStep3') >=0)
+        	{
+        		step = 3;
+        	}
+        	else
+        	{
+        		step = 4;
+        	}
+        	
+        	$('#collapse'+step+' .panel-body').html(data);
+        	$('#collapse'+step).collapse('show');
+        	
+        	
+        	$('#formStep'+step).submit(checkoutSubmitHandler);
+        }
+    });
+
+    ev.preventDefault();
+};
+
 (function ( $ ) {
     'use strict';
 
-    $(document).ready(function() {
+    $(document).ready(function() 
+    {
+    	
+    	$('#formStep1').submit(checkoutSubmitHandler);
 
         $('.sylius-different-billing-address-trigger').click(function() {
             $('#sylius-billing-address-container').toggleClass('hidden');
