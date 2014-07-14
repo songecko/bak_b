5790fdaedf920cb86fb43006a29a591be7991e81
Damian <correo@tije.com>
2014-07-14 16:46:41 -0300

    send email swiftmailer

--- src/Sylius/Bundle/WebBundle/Controller/Frontend/HomepageController.php
+++ src/Sylius/Bundle/WebBundle/Controller/Frontend/HomepageController.php
@@ -54,7 +54,7 @@ class HomepageController extends Controller
     	
     	if($errors->count() == 0)
     	{
-	    	$mailChimp = new MailChimp('fa45ac5e1dbe50997a0b2f475f6400d1-us7');
+	    	/*$mailChimp = new MailChimp('fa45ac5e1dbe50997a0b2f475f6400d1-us7');
 	    	$mailChimp->call('lists/subscribe', array(
 	    			'id'                => 'd53b936801',
 	    			'email'             => array('email'=>$email),
@@ -62,7 +62,27 @@ class HomepageController extends Controller
 	    			'update_existing'   => true,
 	    			'replace_interests' => false,
 	    			'send_welcome'      => false,
-	    	));
+	    	));*/
+	    	
+	    	$message = \Swift_Message::newInstance()
+	    	->setSubject('Newsletter')
+	    	->setFrom('info@brandsofpuertorico.com')
+	    	->setTo($email)
+	    	->setBody(
+		    			'Dear Newsletter Subscriber,
+	
+						Thank you for subscribing to the Brands of Puerto Rico Newsletter. The premier Newsletter for all information concerning the events, information and bi-monthly activities carried out by Brands of Puerto Rico.
+	
+						We thank you for taking interest in what the Brands of Puerto Rico E-commerce team is planning and coordinating to better help our local brands owners.
+						
+						Your email account will now be receiving bi-weekly reports concerning the available brands, bi monthly activities planned by our Brands relations team and of course any and all other prioritized information.
+						
+						
+						Brands of Puerto Rico Team
+						#BOPR#somosmásque100x35'
+	    			);
+	    	
+	    	$this->get('mailer')->send($message);
 	    	
 	    	$flash = 'success';
 	    	$message = 'Confirmed subscription';
