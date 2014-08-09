77b9661c31f176aa4595c8587fa542e0d10d2f61
BigBoss <damian.damico86@gmail.com>
2014-08-09 15:53:31 -0300

    test send email

--- src/Sylius/Bundle/CoreBundle/Checkout/Step/AddressingStep.php
+++ src/Sylius/Bundle/CoreBundle/Checkout/Step/AddressingStep.php
@@ -30,6 +30,53 @@ class AddressingStep extends CheckoutStep
     public function displayAction(ProcessContextInterface $context)
     {
         $order = $this->getCurrentCart();
+        
+        $manufacturers = array();
+        $i = 0;
+        
+        foreach($order->getItems() as $item)
+        {
+        	$manufacturerEmail = $item->getProduct()->getManufacturer()->getEmail();
+        	$itemSale = $item->getProduct()->getName().' cantidad: '.$item->getQuantity()."\n";
+        	
+        	if(!array_key_exists($manufacturerEmail, $manufacturers))
+        	{
+        		$manufacturers[$manufacturerEmail] = $itemSale;
+        	}
+        	else
+        	{
+        		$manufacturers[$manufacturerEmail] .= $itemSale;
+        	}
+        }
+        
+        foreach($manufacturers as $manufacturer => $item)
+        {
+        	$message = \Swift_Message::newInstance()
+	    	->setSubject('Venta')
+	    	->setFrom('info@brandsofpuertorico.com')
+	    	->setTo($manufacturer)
+	    	->setBody(
+		    			'Saludos,
+
+Queremos felicitarte porque has vendido tus productos en Brands of Puerto Rico. Estamos muy emocionados y orgullosos de ustedes y todas las marcas que componen nuestra iniciativa. Esto demuestra que nuestro trabajo junto a sus maravillosos productos SI va a rendir frutos. Entiendo que ya recibieron el pago de Paypal, por favor confirmen.
+
+Esto es una oportunidad para darle a este cliente una experiencia extraordinaria y más aún, demostrarle que en Puerto Rico las cosas se hacen bien y de calidad. Les pedimos que nos ayuden a sacarle provecho a esta oportunidad. Queremos recolectar sus productos lo más pronto posible para enviarlos. Además de sus productos, si tienen algún material promocional está más que bienvenido. 
+
+Gracias por unirte en este emprendimiento y creer en nosotros. Juntos vamos a lograr cosas grandes y demostrar que somos más que 100 x 35, este es solo el comienzo. Confiamos plenamente en ustedes, sus marcas y sus productos, por eso damos y vamos a seguir dando todo nuestro esfuerzo para que la gente los conozca. Para nosotros ustedes son una inspiración para hacer las cosas mejor. 
+
+El producto vendido fue: 
+
+'.$item.' 
+
+
+Cualquier duda saben que estamos a la orden. Esto es un trabajo en equipo, sin ustedes no existiría Brands of Puerto Rico.
+
+¡De nuevo, gracias de parte de todo nuestro equipo!'
+	    			);
+        	
+        	$this->get('mailer')->send($message);
+        }
+        
         $this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_INITIALIZE, $order);
 
         $form = $this->createCheckoutAddressingForm($order);
