a3d2f409172ba368a2cd3674e953984cecb5040c
Pablo Damico <pablo.damico88@gmail.com>
2014-10-19 20:09:30 -0300

    categories rand

---
+++ src/Tresepic/BoprBundle/DQL/Rand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TresEpic\BoprBundle\DQL;
+
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * Mysql RAND() implementation for Doctrine2
+ */
+class Rand extends FunctionNode
+{
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'RAND()';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
